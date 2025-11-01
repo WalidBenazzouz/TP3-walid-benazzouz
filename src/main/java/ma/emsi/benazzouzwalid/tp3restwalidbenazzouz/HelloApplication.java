2a639ed68ef894343554ca92fb75a1eeadf7ccf8
MCwalid <148798330+MCwalid@users.noreply.github.com>
2025-11-01 21:08:08 +0100

    first commit

---
+++ src/main/java/ma/emsi/benazzouzwalid/tp3restwalidbenazzouz/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.benazzouzwalid.tp3restwalidbenazzouz;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
